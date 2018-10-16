b900a824ccae32269a978cd97f5d92461b83cc3e
dntAtMe <dntatm3@gmail.com>
2018-10-16 20:16:29 +0200

    add comments on l1t1

--- list_1/zad1.jl
+++ list_1/zad1.jl
@@ -1,10 +1,13 @@
 types = [Float16, Float32, Float64]
 sigbits = [0, 0, 0]
+
 # Precyzja arytmetyki ma wpływ na to, ile cyfr znaczących jest
 # reprezentowanych dokładnie
 # macheps to najmniejsza dodatnia liczba t. że dodana do jedności
 # da w wyniku liczbę > 1.0
 # SUMMARUM: im mniejsza wartość machepsu, tym większa precyzja arytmetyki
+
+#       macheps
 for idx in eachindex(types)
     val = types[idx](2.0)
     pow = 0
@@ -20,6 +23,7 @@ end
 
 println()
 
+#       eta
 for idx in eachindex(types)
     val = types[idx](1.0)
     pow = 0
@@ -34,6 +38,7 @@ end
 
 println()
 
+#       max
 for idx in eachindex(types)
     val = types[idx](1.0 - (1/2)^sigbits[idx])
     pow = 0
@@ -44,13 +49,4 @@ for idx in eachindex(types)
     println(string(types[idx]))
     println("result: ", val, " | ", pow)
     println("expected: ", realmax(types[idx]))
-end
-
-println()
-
-x = Float16(1.0)
-y = Float16(1.0)
-while(!isinf(Float16(x)))
-    y = x
-    x = Float16(x*2)
 end
